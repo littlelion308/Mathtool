92e4bb2a55d2f05ae6d4c38bb72e0fa9f480caf6
Littlelion308 <littlelion308@gmx.de>
2025-01-22 13:49:37 +0100

    started and than paused matrix...

---
+++ src/matrix.c
@@ -0,0 +1,7 @@
+#include "matrix.h"
+
+/* #define getRowsMatrix(a[1][1]) sizeof(a)/sizeof(a[0]); */
+
+int getColumsMatrix(frac a[1][1]) {
+	return sizeof(a[0])/sizeof(frac);
+}
---
+++ src/matrix.h
@@ -0,0 +1,5 @@
+#include "frac.h"
+
+// This is currently not implemented at all, due to wired behavior wen passing arrays
+// a Matrix is defined as a tow-dimensional Array of frac[rows][column]
+
