f1c3d6b8839114214ba64a65366af79dd51dbf6a
Littlelion308 <littlelion308@gmx.de>
2025-04-04 11:10:09 +0200

    rewrote polynom accordingly

--- src/polynom.c
+++ src/polynom.c
@@ -1,104 +1,237 @@
 #include "polynom.h"
+#include <stdlib.h>
+#include <string.h>
+
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
-polynom makePolynom(num a[], int deg) {
+const ring getPolynomRing(field *f) {
+	polynom addneutural = makePolynom(f->add.neutral,0);
+	group fracAdd = {&addPolynom, &addneutural, &addInvertPolynom, &subPolynom};
+	polynom mplneutural = makePolynom(f->mpl.neutral,1);
+	monoid fracMpl = {&mplPolynom, &mplneutural};
+	const genops gOps = {&optimisePolynom, &copyPolynom, &freePolynom};
+	ring fracField = {fracAdd, fracMpl, gOps, "Polynom R"};
+	return fracField;
+}
+
+void *copyPolynom(void *vf){
+	polynom *f = (polynom *)vf;
+	field bf = *(f->p[0].field);
+	f = (polynom *)optimisePolynom(f);
+	polynom *g = malloc(sizeof(unsigned int) + sizeof(fieldVar) * (f->deg + 1));
+	g->deg = f->deg;
+	for (int i = 0; i < f->deg; i++)
+		g->p[i] = *(fieldVar *)bf.gen.copy(&f->p[i]);
+	return g;
+}
+
+void freePolynom(void *vf) {
+	polynom *f = (polynom *)vf;
+	for (int i = 0; i <= f->deg; i++)
+		varFree(&(f->p[i]));
+	free(f);
+}
+
+void *optimisePolynom(void *vf) {
+	polynom *f = (polynom *)((ringVar *)vf)->val;
+	// optimize The Numbers
+	for (int i = 0; i <= f->deg; i++)
+		f->p[0].field->gen.optimise(&f->p[i]);
+	// optimize the size
+	int i = f->deg;
+	while (i > 0 && f->p[i].val == f->p[0].field->add.neutral)
+		i--;
+	polynom *g = malloc(sizeof(unsigned int) + sizeof(fieldVar) * (i + 1));
+	g->deg = i;
+	memcpy(g->p, f->p, sizeof(fieldVar) * (i +1));
+	freePolynom(f);
+	fieldVar *rf = (fieldVar *)vf;
+	rf->val = g;
+	return rf;
+}
+
+
+polynom makePolynom(fieldVar a[], int deg) {
 	polynom f;
 	f.deg = deg;
 	for (; deg > 0; deg--) {
 		f.p[deg] = a[deg];
 	}
+	f = *(polynom *)optimisePolynom(&f);
 	return f;
 }
 
 
-num calcPolynom(polynom f, num x) {
-	num value = makeNum(0,1);
-	for (int i = 0; i <= f.deg; i++) {
-		value = optimiseNum(addNum(value, mplNum(f.p[i], powerNumInt(x,i))));
+fieldVar *calcPolynom(ringVar *vf, void *vx) {
+	polynom *f = vf->val;
+	fieldVar *x = (fieldVar *)vx;
+	fieldVar neut = {x->field->add.neutral, x->field};
+	fieldVar *value = &neut;
+	for (int i = 0; i <= f->deg; i++) {
+		fieldVar *toadd = x->field->mpl.neutral;
+		for (int j = 1; j <= i; j++) {
+			fieldVar *b = x->field->mpl.op(x, toadd);
+			free (toadd);
+			toadd = b;
+		}
+		fieldVar *b = x->field->add.op(value, x->field->mpl.op(&(f->p[i]), toadd));
+		free(value);
+		value = b;
 	}
+	x->field->gen.optimise(&value);
 	return value;
 }
 
-polynom addPolynom(polynom f, polynom g) {
+void *addPolynom(void *vf, void *vg) {
+	polynom *f = (polynom *)((ringVar *)vf)->val;
+	polynom *g = (polynom *)((ringVar *)vg)->val;
+	field bf = *(f->p[0].field);
 	polynom h;
-	h.deg = MAX(f.deg,g.deg);
+	h.deg = MAX(f->deg,g->deg);
 	int i = 0;
-	for (;  i < MIN(f.deg,g.deg);i++) {
-		h.p[i] = optimiseNum(addNum(f.p[i], g.p[i]));
+	for (;  i < MIN(f->deg,g->deg);i++) {
+
+		h.p[i].val = ((bf.add.op((f->p[i].val), (g->p[i]).val)));
+		bf.gen.optimise(&h.p[i]);
+		h.p[i].field = (f->p[0].field);
 	}
 	for (; i <= h.deg; i++ ) {
-		if (f.deg == h.deg)
-			h.p[i] = f.p[i];
+		if (f->deg == h.deg)
+			h.p[i] = f->p[i];
 		else
-			h.p[i] = g.p[i];
+			h.p[i] = g->p[i];
 	}
-	num j = h.p[h.deg];
-	while (optimiseNum(j).numerator == 0) {
-		h.deg--;
-		j = h.p[h.deg];
-	}
-	return h;
+	void *j = copyPolynom(&h);
+	j = (polynom *)optimisePolynom(j);
+	ringVar *rj;
+	rj->val = j;
+	rj->ring = ((ringVar *)vf)->ring;
+	return rj;
 }
 
-polynom mplPolynom(polynom f, polynom g) {
+
+void *addInvertPolynom(void *f) {
+	ringVar *rg = (ringVar *)varCopy(f);
+	polynom *g = rg->val;
+	field bf = *(g)->p[0].field;
+	for (int i = 0; i <= g->deg; i++) {
+		fieldVar *a = bf.add.invert( &(g->p[i]));
+		varFree(&(g->p[i]));
+		g->p[i] = *a;
+		varFree(a);
+	}
+
+	return g;
+}
+
+
+void *subPolynom(void *f, void *g) {
+	ringVar *h = addInvertPolynom(g);
+	ringVar *i = addPolynom(f,h);
+	varFree(h);
+	return i;
+}
+
+void *mplPolynom(void *vf, void *vg) {
+	polynom *f = (polynom *)((ringVar *)vf)->val;
+	polynom *g = (polynom *)((ringVar *)vg)->val;
+	field bf = *(f->p[0].field);
 	polynom h;
-	h.deg = f.deg + g.deg;
+	h.deg = f->deg + g->deg;
 	for (int i = 0; i <= h.deg; i++) {
-		num value;
-		for (int j = 0; j <= f.deg; j++) {
-			for (int k = 0; k <= g.deg; k++) {
-				if (j + k == i)
-					value = optimiseNum(addNum(value, mplNum(optimiseNum(f.p[j]), optimiseNum(g.p[k]))));
+		fieldVar value;
+		for (int j = 0; j <= f->deg; j++) {
+			for (int k = 0; k <= g->deg; k++) {
+				if (j + k == i) {
+					fieldVar *c = bf.mpl.op(&f->p[i], &g->p[i]);
+					fieldVar *b = bf.add.op(&value, &c);
+					value = *b;
+					free(b);
+					free(c);
+				}
 			}
 		}
 		h.p[i] = value;
 	}
-	num i = h.p[h.deg];
-	while (optimiseNum(i).numerator == 0) {
-		h.deg--;
-		i = h.p[h.deg];
-	}
-	return h;
+	void *j = copyPolynom(&h);
+	ringVar *rj;
+	rj->val = j;
+	rj->ring = ((ringVar *)vf)->ring;
+	return rj;
 }
 
-polynom derivePolynom(polynom f) {
+ringVar *derivePolynom(ringVar *rf) {
+	polynom *f = rf->val;
+	if (strcmp(f->p[0].field->name, "Rational F") != 0 || strcmp(f->p[0].field->name, "Real F") != 0 )
+		return NULL;
 	polynom h;
-	h.deg = f.deg - 1;
-	for (int i = f.deg; i > 0; i--) {
-		h.p[i - 1] = optimiseNum(mplNum(f.p[i] , makeNum(i, 1)));
+	h.deg = f->deg - 1;
+	for (int i = f->deg; i > 0; i--) {
+		fieldVar *inta;
+		if (strcmp(f->p[0].field->name, "Rational F") == 0)
+			inta = makeFracFromInts(i, 1);
+		h.p[i - 1] = *(fieldVar *)inta->field->mpl.op(inta, &f->p[i]);
+		varFree(inta);
 	}
-	return h;
+	void *j = copyPolynom(&h);
+	ringVar *rj;
+	rj->val = j;
+	rj->ring = rf->ring;
+	return rj;
 
 }
 
-polynom intergrate(polynom f) {
+ringVar *intergrate(ringVar *rf) {
+	polynom *f = rf->val;
+	if (strcmp(f->p[0].field->name, "Rational F") != 0 || strcmp(f->p[0].field->name, "Real F") != 0 )
+		return NULL;
 	polynom F;
-	F.deg = f.deg + 1;
-	for (int i = F.deg; i > 0; i--) {
-		F.p[i] = optimiseNum(mplNum(f.p[i-1], makeNum(1, i)));
+	F.deg = f->deg + 1;
+	for (int i = F.deg - 1; i >= 0; i--) {
+		fieldVar *inta;
+		if (strcmp(f->p[0].field->name, "Rational F") == 0)
+			inta = makeFracFromInts(1, i);
+		F.p[i + 1] = *(fieldVar *)inta->field->mpl.op(inta, &f->p[i]);
+		varFree(inta);
 	}
-	F.p[0] = makeNum(0,1);
-	return F;
+	if (strcmp(f->p[0].field->name, "Rational F") == 0)
+		F.p[0] = *makeFracFromInts(0,1);
+	void *j = copyPolynom(&F);
+	ringVar *rj;
+	rj->val = j;
+	rj->ring = rf->ring;
+	return rj;
 }
 
-divisoremainder divPolynom(polynom f, polynom g) {
-	divisoremainder ret;
+divisorremainder divPolynom(ringVar *rf, ringVar *rg) {
+	polynom *f = rf->val;
+	polynom *g = rg->val;
+	field bf = *(f->p[0].field);
+	divisorremainder ret;
 	polynom res;
-	res.deg = f.deg - g.deg;
-	num negNumAr[] = {makeNum(-1,1)};
-	polynom neg = makePolynom(negNumAr, 0);
-	while (f.deg >= g.deg) {
-		res.p[g.deg-f.deg] = optimiseNum(divfullNum( f.p[f.deg] , g.p[g.deg]));
-		f = addPolynom(mplPolynom(neg,f), g);
+	res.deg = f->deg - g->deg;
+	polynom *h = copyPolynom(f);
+	while (h->deg >= g->deg) {
+		res.p[g->deg - h->deg] = *(fieldVar *)bf.mpl.invOp( &h->p[h->deg] , &g->p[g->deg]);
+		polynom *i = subPolynom(h, g);
+		freePolynom(h);
+		h = i;
 	}
-	ret.f = res;
-	ret.remainder = f;
-	num i = f.p[f.deg];
-	while (optimiseNum(i).numerator == 0) {
-		f.deg--;
-		i = f.p[f.deg];
-	}
+	void *j = copyPolynom(&res);
+	ringVar rres;
+	rres.val = j;
+	rres.ring = rf->ring;
+	ret.divisor = rres;
+
+	void *k = copyPolynom(&h);
+	ringVar rrem;
+	rrem.val = j;
+	rrem.ring = rf->ring;
+	ret.remainder = rrem;
+
+	optimisePolynom(ret.remainder.val);
+	optimisePolynom(ret.divisor.val);
 
 	return ret;
 
--- src/polynom.h
+++ src/polynom.h
@@ -1,36 +1,61 @@
-#include "num.h"
-#include <stdbool.h>
+#include "field.h"
+#include "ring.h"
+#include "divableRing.h"
+#include "genericVar.h"
+
+// For deriving / Integrating
+#include "frac.h"
 
 #ifndef POLYNOM
 #define POLYNOM
 typedef struct {
-	int deg;
-	num p[];
+	unsigned int deg;
+	fieldVar p[];
 } polynom;
 // polynom a;
 // a.p = {0,1,2,3,4}
 // => f(x) ^= 4x^4 + 3x^3 + 2x^2 + 1x + 0
-typedef struct {
-	bool remIsNull;
-	polynom remainder;
-	polynom f;
-} divisoremainder;
 #endif
 
+//////////// Specific
 ////// general operations on any Ring, here in Polynoms over realnum////
-// Turns the list of descending nums and the deg into a polynom.
-polynom makePolynom(num a[], int deg);
-// adds the tow given Polynoms together.
-polynom addPolynom(polynom f, polynom g);
-// multiplies the tow given Polynoms.
-polynom mplPolynom(polynom f, polynom g);
 // divide Polynoms
-divisoremainder divPolynom(polynom numerator, polynom denominator);
+divisorremainder divPolynom(ringVar *numerator, ringVar *denominator);
 
 ////// Polynom or Function specific functions
-// Calculates the value of a given polynom a location x.
-num calcPolynom(polynom f, num x);
-// calculates the derivative of f, usualy calles f'
-polynom derivePolynom(polynom f);
+// calculates the derivative of f, usualy calles f' // Only works over Real and Rational Numbers
+ringVar *derivePolynom(ringVar *f);
 // Calculates the standard Integral of f, commonly called F
-polynom intergrate(polynom f);
+ringVar *intergrate(ringVar *f);
+
+
+
+
+
+
+
+// general fkjdhaskjflhasl
+//// generic
+const ring getPolynomRing(field *f);
+void *copyPolynom(void *f);
+void freePolynom(void *f);
+void *optimisePolynom(void *f);
+
+//// Add
+// adds the tow given Polynoms together.
+void *addPolynom(void *f, void *g);
+
+void *addInvertPolynom(void *f);
+void *subPolynom(void *f, void *g);
+
+//// Mpl
+// multiplies the tow given Polynoms.
+void *mplPolynom(void *f, void *g);
+
+
+/////// Specific
+// Turns the list of descending nums and the deg into a polynom.
+polynom makePolynom(fieldVar a[], int deg);
+
+// Calculates the value of a given polynom a location x.
+fieldVar *calcPolynom(ringVar *f, void *x);
